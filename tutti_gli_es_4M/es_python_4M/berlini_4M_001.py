fd5021550596d4094580232de8be2dd66c3a1ea9
AngeloBerlini <angelo.berlini@studenti.isissgobetti.it>
2024-11-04 09:14:58 +0100

    jfhdsjkfdsh

---
+++ tutti_gli_es_4M/es_python_4M/berlini_4M_001.py
@@ -0,0 +1,17 @@
+#ESERCIZIO 1
+
+class Persona:
+    def __init__(self, nome : str, eta : int, citta : str) -> None:
+        self.nome = nome
+        self.eta = eta
+        self.citta = citta
+    
+    def saluta(self) -> None:
+        print (f"Ciao, mi chiamo {self.nome}")
+
+    def descrizione(self) -> None:
+        print (f"Ho {self.eta} anni e vivo a {self.citta}")
+
+persona = Persona("Giorgio", 30, "Misano")
+persona.saluta()
+persona.descrizione()
